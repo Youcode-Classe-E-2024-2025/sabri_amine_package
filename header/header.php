091998fa758cd00fb5992fa14bc284de86de376d
Sabri Amine <124732319+sabri-amine@users.noreply.github.com>
2024-12-09 16:10:39 +0100

    j'ai ajouté fichier header dans dossier header

---
+++ header/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../assets/src/output.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Document</title>
+</head>
+<body>
+    
+        
+    <header class="flex justify-between items-center pr-10 pl-10  shadow-lg">
+        
+        <div class="text-xl font-bold">
+        <img src="../assets/images/logo_js.png" alt="img js not found" width = "60px">
+        </div>
+
+        <div class="space-x-4">
+            <button class="bg-yellow-500 text-white px-4 py-1 rounded hover:bg-yellow-600">Login</button>
+            <button class="bg-gray-700  text-white px-4 py-1 rounded hover:bg-gray-500">Logout</button>
+        </div>
+    </header>
+
+</body>
+</html>
