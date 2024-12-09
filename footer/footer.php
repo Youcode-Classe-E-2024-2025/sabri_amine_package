722f3edaf1f4a9a4a525d2b6fc09b25bac552487
Sabri Amine <124732319+sabri-amine@users.noreply.github.com>
2024-12-09 17:30:10 +0100

    j'ai ajouté footer mais n'est pas terminé

---
+++ footer/footer.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/src/output.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+</head>
+<body>
+<footer class="  py-10 border-2 border-gray-500">
+    <div class="flex justify-between items-center max-w-7xl mx-auto px-4">
+        <div>
+            <img src="../assets/images/logo_js.png" alt="Logo JavaScript" width="60px">
+        </div>
+        <div class="flex space-x-4">
+            <a href="#" target="_blank" class="text-blue-600 ">
+            <i class="bi bi-facebook"></i>
+            </a>
+            <a href="#" target="_blank" class="text-gray-900 ">
+                <i class="bi bi-twitter-x"></i>
+            </a>
+            <a href="#" target="_blank" class="text-blue-500 ">
+                <i class="bi bi-linkedin"></i>
+            </a>
+        </div>
+    </div>
+    <div class="text-sm">
+       <p> &copy 2024 Gestion de packeges.</p>
+    </div>
+</footer>
+</body>
+</html>
--- index.php
+++ index.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./assets/src/output.css">
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>Document</title>
 </head>
 <body>
-    
+    <?php include('./footer/footer.php') ?>
 </body>
 </html>
