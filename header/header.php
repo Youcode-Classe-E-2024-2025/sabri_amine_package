d77c46363bb573a93af679c0715035d4e076b0bb
Sabri Amine <124732319+sabri-amine@users.noreply.github.com>
2024-12-09 16:12:28 +0100

    supprimé les espace de fichier header

--- header/header.php
+++ header/header.php
@@ -7,13 +7,10 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <title>Document</title>
 </head>
-<body>
-    
-        
+<body>   
     <header class="flex justify-between items-center pr-10 pl-10  shadow-lg">
-        
         <div class="text-xl font-bold">
-        <img src="../assets/images/logo_js.png" alt="img js not found" width = "60px">
+            <img src="../assets/images/logo_js.png" alt="img js not found" width = "60px">
         </div>
 
         <div class="space-x-4">
@@ -21,6 +18,5 @@
             <button class="bg-gray-700  text-white px-4 py-1 rounded hover:bg-gray-500">Logout</button>
         </div>
     </header>
-
 </body>
 </html>
