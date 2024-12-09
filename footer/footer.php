eeebcc3169caabefa8f2b88931b34d1d32096c8b
Sabri Amine <124732319+sabri-amine@users.noreply.github.com>
2024-12-09 18:58:40 +0100

    j'ai terminé footer et include dans la page index

--- footer/footer.php
+++ footer/footer.php
@@ -1,33 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./assets/src/output.css">
-    <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-</head>
-<body>
-<footer class="  py-10 border-2 border-gray-500">
-    <div class="flex justify-between items-center max-w-7xl mx-auto px-4">
-        <div>
-            <img src="../assets/images/logo_js.png" alt="Logo JavaScript" width="60px">
+    <footer class="bg-gray-100 text-white py-6 shadow-lg">
+        <div class="flex justify-between items-center max-w-7xl mx-auto px-8">
+            <div>
+                <img src="./assets/images/logo_js.png" alt="Logo JavaScript" class="w-16">
+            </div>
+            <div class="flex space-x-6">
+                <a href="#" target="_blank" class="text-gray-400 hover:text-blue-600">
+                    <i class="bi bi-facebook text-2xl"></i>
+                </a>
+                <a href="#" target="_blank" class="text-gray-400 hover:text-blue-400">
+                    <i class="bi bi-twitter text-2xl"></i>
+                </a>
+                <a href="#" target="_blank" class="text-gray-400 hover:text-blue-500">
+                    <i class="bi bi-linkedin text-2xl"></i>
+                </a>
+            </div>
         </div>
-        <div class="flex space-x-4">
-            <a href="#" target="_blank" class="text-blue-600 ">
-            <i class="bi bi-facebook"></i>
-            </a>
-            <a href="#" target="_blank" class="text-gray-900 ">
-                <i class="bi bi-twitter-x"></i>
-            </a>
-            <a href="#" target="_blank" class="text-blue-500 ">
-                <i class="bi bi-linkedin"></i>
-            </a>
+        <div class="text-center mt-6 text-gray-400 text-sm">
+            <p>&copy; 2024 Gestion de packages. Tous droits réservés.</p>
         </div>
-    </div>
-    <div class="text-sm">
-       <p> &copy 2024 Gestion de packeges.</p>
-    </div>
-</footer>
-</body>
-</html>
+    </footer>
+
