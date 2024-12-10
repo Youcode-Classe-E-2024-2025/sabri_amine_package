b329410329586dd3e883ef34849b5f4bd6d77bf5
Sabri Amine <124732319+sabri-amine@users.noreply.github.com>
2024-12-10 15:08:56 +0100

    j'ai ajouté fichier admin dans dossier admin

--- header/header.php
+++ header/header.php
@@ -1,6 +1,6 @@
     <header class="flex justify-between items-center pr-10 pl-10  shadow-lg">
         <div class="text-xl font-bold">
-            <img src="./assets/images/logo_js.png" alt="img js not found" width = "60px">
+            <img src="../assets/images/logo_js.png" alt="img js not found" width = "60px">
         </div>
 
         <div class="space-x-4">
